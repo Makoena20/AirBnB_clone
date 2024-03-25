9ae5f3fe33a0216517b9d76b8f5fefb33fbaeb25
Makoena20 <koenaletseku@gmail.com>
2024-03-25 06:35:14 -0700

    models

---
+++ models/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module for User class.
+"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """
+    User class that inherits from BaseModel.
+    Attributes:
+        email (str): empty string
+        password (str): empty string
+        first_name (str): empty string
+        last_name (str): empty string
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
