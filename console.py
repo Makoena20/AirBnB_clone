868af67eca92fd7c9757106a0a3c11317e467041
Makoena20 <koenaletseku@gmail.com>
2024-03-25 06:39:19 -0700

    models

--- console.py
+++ console.py
@@ -1,24 +1,145 @@
-#!/usr/bin/env python3
-"""Module for the HBNB command interpreter."""
+#!/usr/bin/python3
+"""
+Console module for Airbnb clone project.
+"""
 import cmd
+from models.base_model import BaseModel
+from models.user import User
+from models.engine.file_storage import FileStorage
+import shlex
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter class."""
-    
-    prompt = '(hbnb) '
+    """
+    HBNBCommand class, a subclass of cmd.Cmd for command interpreter.
+    """
+    prompt = "(hbnb) "
+    classes = {"BaseModel": BaseModel, "User": User}
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id. Ex: $ create BaseModel
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg not in self.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = self.classes[arg]()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class name
+        and id. Ex: $ show BaseModel 1234-1234-1234.
+        """
+        args = shlex.split(arg)
+        if not arg:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        key = args[0] + '.' + args[1]
+        objs = FileStorage().all()
+        if key in objs:
+            print(objs[key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id (saves the change
+        into the JSON file). Ex: $ destroy BaseModel 1234-1234-1234.
+        """
+        args = shlex.split(arg)
+        if not arg:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        key = args[0] + '.' + args[1]
+        objs = FileStorage().all()
+        if key in objs:
+            del objs[key]
+            FileStorage().save()
+        else:
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the
+        class name. Ex: $ all BaseModel or $ all.
+        """
+        objs = FileStorage().all()
+        if arg:
+            args = shlex.split(arg)
+            if args[0] not in self.classes:
+                print("** class doesn't exist **")
+                return
+            print([str(objs[key]) for key in objs.keys() if
+                   key.startswith(args[0])])
+        else:
+            print([str(objs[key]) for key in objs.keys()])
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or
+        updating attribute (saves the change into the JSON file).
+        Ex: $ update BaseModel 1234-1234-1234 email "aibnb@holbertonschool.com".
+        """
+        args = shlex.split(arg)
+        if not arg:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        key = args[0] + '.' + args[1]
+        objs = FileStorage().all()
+        if key not in objs:
+            print("** no instance found **")
+            return
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        if len(args) == 3:
+            print("** value missing **")
+            return
+        setattr(objs[key], args[2], args[3])
+        FileStorage().save()
 
     def do_quit(self, arg):
-        """Quit command to exit the program."""
+        """
+        Quit command to exit the program.
+        """
         return True
 
     def do_EOF(self, arg):
-        """Exit the program."""
-        print()
+        """
+        EOF command to exit the program.
+        """
         return True
 
     def emptyline(self):
-        """Do nothing on empty line."""
+        """
+        Overwrites emptyline method to prevent repeating the last non-empty
+        command when the empty line is entered.
+        """
         pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,41 +1,49 @@
 #!/usr/bin/python3
-"""This module defines the FileStorage class."""
-
+"""
+Module for FileStorage class.
+"""
 import json
 
-
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances."""
-
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances.
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
-        return self.__objects
+        """
+        Returns the dictionary __objects.
+        """
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
+        """
+        Serializes __objects to the JSON file (path: __file_path).
+        """
         new_dict = {}
-        for key, value in self.__objects.items():
+        for key, value in FileStorage.__objects.items():
             new_dict[key] = value.to_dict()
-        with open(self.__file_path, mode='w', encoding="utf-8") as f:
-            json.dump(new_dict, f)
+        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as file:
+            json.dump(new_dict, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
+        """
+        Deserializes the JSON file to __objects (only if the JSON file exists).
+        """
         try:
-            with open(self.__file_path, mode='r', encoding="utf-8") as f:
-                json_dict = json.load(f)
-                for key, value in json_dict.items():
+            with open(FileStorage.__file_path, mode='r', encoding='utf-8') as file:
+                objs_dict = json.load(file)
+                for key, value in objs_dict.items():
                     class_name, obj_id = key.split('.')
-                    cls = eval(class_name)
-                    self.__objects[key] = cls(**value)
+                    self.new(eval(class_name)(**value))
         except FileNotFoundError:
             pass
 
