2663da076c348fe826c55b42391d7452d9f73f97
Makoena20 <koenaletseku@gmail.com>
2024-03-25 05:08:29 -0700

    models

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,18 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -21,4 +29,15 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
+# Testing code
+if __name__ == "__main__":
+    my_model1 = BaseModel()
+    print(my_model1)
+
+    my_model2 = BaseModel()
+    print(my_model2)
+
+    my_model3 = BaseModel(**my_model1.to_dict())
+    print(my_model3)
 
