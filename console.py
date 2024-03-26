40dea4c8f9c999971c0fd1040fb27a8cf9008217
Makoena20 <koenaletseku@gmail.com>
2024-03-26 01:50:34 -0700

    console.py

--- console.py
+++ console.py
@@ -18,26 +18,5 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing when an empty line is entered"""
         pass
 
-    def do_create(self, arg):
-        """Create a new instance of BaseModel"""
-        print("create", arg)
-
-    def do_show(self, arg):
-        """Show the string representation of an instance"""
-        print("show", arg)
-
-    def do_destroy(self, arg):
-        """Delete an instance based on the class name and id"""
-        print("destroy", arg)
-
-    def do_all(self, arg):
-        """Print all string representations of all instances"""
-        print("all", arg)
-
-    def do_update(self, arg):
-        """Update an instance based on the class name and id"""
-        print("update", arg)
-
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
