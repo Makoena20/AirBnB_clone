a499dee14030ea2c2841a73e98ab14a79a536ddd
Makoena20 <koenaletseku@gmail.com>
2024-04-03 05:34:53 -0700

    console.py

--- console.py
+++ console.py
@@ -1,36 +1,149 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-This module contains the entry point of the command interpreter.
+Console Module
 """
-
 import cmd
+import shlex
+import models
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
-    """
-    Command interpreter class.
-    """
+    """Command Interpreter"""
 
-    prompt = '(hbnb) '
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """
-        Quit command to exit the program.
-        """
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """
-        Exit the program gracefully at end of file.
-        """
-        print()  # Print a newline before exiting
+        """EOF signal to exit the program"""
         return True
 
     def emptyline(self):
-        """
-        Called when an empty line is entered.
-        """
+        """Empty line + ENTER shouldn't execute anything"""
         pass
 
-if __name__ == '__main__':
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(args[0])()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        try:
+            objects = models.storage.all()
+            key = args[0] + "." + args[1]
+            print(objects[key])
+        except KeyError:
+            print("** no instance found **")
+        except AttributeError:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        try:
+            objects = models.storage.all()
+            key = args[0] + "." + args[1]
+            del objects[key]
+            models.storage.save()
+        except KeyError:
+            print("** no instance found **")
+        except AttributeError:
+            print("** class doesn't exist **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        objects = models.storage.all()
+        if arg:
+            try:
+                objs = [str(obj) for obj in objects.values() if obj.__class__.__name__ == arg]
+                print(objs)
+                return
+            except NameError:
+                print("** class doesn't exist **")
+                return
+        print([str(obj) for obj in objects.values()])
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        if len(args) == 3:
+            print("** value missing **")
+            return
+        try:
+            objects = models.storage.all()
+            key = args[0] + "." + args[1]
+            obj = objects[key]
+            setattr(obj, args[2], args[3])
+            models.storage.save()
+        except KeyError:
+            print("** no instance found **")
+        except AttributeError:
+            print("** class doesn't exist **")
+
+    def default(self, line):
+        """Default behavior"""
+        commands = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "update": self.do_update
+        }
+        cmd = line.split(".")
+        if len(cmd) == 2:
+            command, args = cmd
+            if command in commands:
+                commands[command](args)
+            elif command == "count":
+                args = cmd[1].split('"')[0]
+                objects = models.storage.all()
+                count = sum(1 for obj in objects.values() if obj.__class__.__name__ == args)
+                print(count)
+            else:
+                print("*** Unknown syntax: {}".format(line))
+        else:
+            print("*** Unknown syntax: {}".format(line))
+
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,48 +1,45 @@
 #!/usr/bin/python3
-"""
-Module for FileStorage class.
-"""
+"""File storage module"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file and deserializes JSON file to instances.
-    """
+    """Serializes instances to a JSON file and deserializes JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns the dictionary __objects.
-        """
+        """Returns the dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Sets in __objects the obj with key <obj class name>.id.
-        """
+        """Sets in __objects the obj with key <obj class name>.id"""
         key = obj.__class__.__name__ + "." + obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes __objects to the JSON file (path: __file_path).
-        """
+        """Serializes __objects to the JSON file"""
         new_dict = {}
         for key, value in FileStorage.__objects.items():
             new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as file:
+        with open(FileStorage.__file_path, "w") as file:
             json.dump(new_dict, file)
 
     def reload(self):
-        """
-        Deserializes the JSON file to __objects (only if the JSON file exists).
-        """
+        """Deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, mode='r', encoding='utf-8') as file:
-                objs_dict = json.load(file)
-                for key, value in objs_dict.items():
-                    class_name, obj_id = key.split('.')
+            with open(FileStorage.__file_path, "r") as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name = value['__class__']
+                    del value['__class__']
                     self.new(eval(class_name)(**value))
         except FileNotFoundError:
             pass
