7206fe3fd95b7589129fb588488cfec453bcbf4f
Makoena20 <koenaletseku@gmail.com>
2024-04-03 06:19:40 -0700

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,35 @@
-#!/usr/bin/python3
-
 import uuid
 from datetime import datetime
 
 class BaseModel:
+    """Defines the BaseModel class."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance."""
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
     def save(self):
+        """Update the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Return a dictionary representation of the object."""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
         return obj_dict
 
-# Testing code
-if __name__ == "__main__":
-    my_model1 = BaseModel()
-    print(my_model1)
-
-    my_model2 = BaseModel()
-    print(my_model2)
-
-    my_model3 = BaseModel(**my_model1.to_dict())
-    print(my_model3)
+    def __str__(self):
+        """Return the string representation of the object."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
