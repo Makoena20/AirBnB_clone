4c2f9817ba073f08b6b771f375af1c84691ce3b8
Makoena20 <koenaletseku@gmail.com>
2024-05-06 06:54:01 -0700

    file_storage.py

--- engine/file_storage.py
+++ engine/file_storage.py
@@ -1,27 +1,44 @@
-class FileStorage:
-    """File storage class"""
+#!/usr/bin/python3
+"""
+This module serializes instances to a JSON file and deserializes JSON file to instances.
+"""
+import json
 
-    def __init__(self):
-        """Initializes the file storage"""
+
+class FileStorage:
+    """
+    This class serializes instances to a JSON file and deserializes JSON file to instances.
+    """
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        """Retrieve all objects"""
+        """Returns the dictionary __objects."""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Add new object"""
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """Save objects to file"""
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        obj_dict = {}
+        for key, value in FileStorage.__objects.items():
+            obj_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
-        """Reload objects from file"""
+        """Deserializes the JSON file to __objects."""
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    module = __import__("models." + class_name, fromlist=[class_name])
+                    class_ = getattr(module, class_name)
+                    FileStorage.__objects[key] = class_(**value)
+        except FileNotFoundError:
+            pass
 
-
-class TestFileStorage(unittest.TestCase):
-    """Test cases for the file storage"""
-
-    def setUp(self):
-        """Set up the test"""
-
-    def tearDown(self):
-        """Clean up after the test"""
