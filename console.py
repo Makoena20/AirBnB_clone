35ef412c3f4e18f4836a83e5519c2cda26285515
Makoena20 <koenaletseku@gmail.com>
2024-05-06 05:38:24 -0700

    console.py

--- console.py
+++ console.py
@@ -1,34 +1,49 @@
 #!/usr/bin/python3
+"""
+Module for the HBNBCommand class.
+"""
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """
+    Command interpreter class.
+    """
+
     prompt = "(hbnb) "
 
-    def do_create(self, arg):
-        # Implement create command logic here
-        pass
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program.
+        """
+        return True
 
-    def do_show(self, arg):
-        # Implement show command logic here
-        pass
-
-    def do_destroy(self, arg):
-        # Implement destroy command logic here
-        pass
-
-    def do_all(self, arg):
-        # Implement all command logic here
-        pass
-
-    def do_update(self, arg):
-        # Implement update command logic here
-        pass
+    def do_EOF(self, arg):
+        """
+        Exit the program.
+        """
+        print()
+        return True
 
     def emptyline(self):
+        """
+        Do nothing on empty line.
+        """
         pass
 
-    def do_EOF(self, line):
-        return True
+    def help_quit(self):
+        """
+        Quit command to exit the program
+        """
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        """
+        Exit the program
+        """
+        print("Exit the program")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
