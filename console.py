8df81656daa0a19c5aee25f3b1334c95d87a77d1
Makoena20 <koenaletseku@gmail.com>
2024-05-06 05:33:48 -0700

    console.py

--- console.py
+++ console.py
@@ -1,125 +1,34 @@
-#!/usr/bin/env python3
-"""Console Module"""
+#!/usr/bin/python3
 import cmd
-import sys
-from models.base_model import BaseModel
-from models import storage
 
 class HBNBCommand(cmd.Cmd):
-    """Command Interpreter"""
-    prompt = '(hbnb) '
-
-    def emptyline(self):
-        """Do nothing on empty input line"""
-        pass
+    prompt = "(hbnb) "
 
     def do_create(self, arg):
-        """Create a new instance of BaseModel"""
-        if not arg:
-            print("** class name missing **")
-            return
-        try:
-            new_instance = eval(arg)()
-            new_instance.save()
-            print(new_instance.id)
-        except NameError:
-            print("** class doesn't exist **")
+        # Implement create command logic here
+        pass
 
     def do_show(self, arg):
-        """Prints the string representation of an instance"""
-        args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-        try:
-            cls = eval(args[0])
-        except NameError:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        objs = storage.all(cls)
-        obj_id = args[1]
-        key = "{}.{}".format(cls.__name__, obj_id)
-        if key in objs:
-            print(objs[key])
-        else:
-            print("** no instance found **")
+        # Implement show command logic here
+        pass
 
     def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id"""
-        args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-        try:
-            cls = eval(args[0])
-        except NameError:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        objs = storage.all(cls)
-        obj_id = args[1]
-        key = "{}.{}".format(cls.__name__, obj_id)
-        if key in objs:
-            del objs[key]
-            storage.save()
-        else:
-            print("** no instance found **")
+        # Implement destroy command logic here
+        pass
 
     def do_all(self, arg):
-        """Prints all string representation of all instances"""
-        if not arg:
-            print("[{}]".format(", ".join(str(obj) for obj in storage.all().values())))
-            return
-        try:
-            cls = eval(arg)
-        except NameError:
-            print("** class doesn't exist **")
-            return
-        print("[{}]".format(", ".join(str(obj) for key, obj in storage.all(cls).items())))
+        # Implement all command logic here
+        pass
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and id"""
-        args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-        try:
-            cls = eval(args[0])
-        except NameError:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        objs = storage.all(cls)
-        obj_id = args[1]
-        key = "{}.{}".format(cls.__name__, obj_id)
-        if key not in objs:
-            print("** no instance found **")
-            return
-        if len(args) < 3:
-            print("** attribute name missing **")
-            return
-        if len(args) < 4:
-            print("** value missing **")
-            return
-        setattr(objs[key], args[2], args[3].strip('"'))
-        storage.save()
+        # Implement update command logic here
+        pass
 
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
+    def emptyline(self):
+        pass
 
-    def do_EOF(self, arg):
-        """EOF signal to exit the program"""
-        print()
+    def do_EOF(self, line):
         return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
