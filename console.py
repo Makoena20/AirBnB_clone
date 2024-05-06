ebd97a03b67697d591073e3be37f290f7b981c5d
Makoena20 <koenaletseku@gmail.com>
2024-05-06 05:48:28 -0700

    console.py

--- console.py
+++ console.py
@@ -44,6 +44,136 @@ class HBNBCommand(cmd.Cmd):
         print("Exit the program")
 
 
+
+
+
+
+
+
+
+"""Console module for AirBnB project."""
+import cmd
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program."""
+        print()
+        return True
+
+    def emptyline(self):
+        """Called when an empty line is entered."""
+        pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel, save it to JSON file, and print its id."""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(args[0])()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class name and id."""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        try:
+            instances = storage.all()
+            key = "{}.{}".format(args[0], args[1])
+            print(instances[key])
+        except KeyError:
+            print("** no instance found **")
+        except AttributeError:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        try:
+            instances = storage.all()
+            key = "{}.{}".format(args[0], args[1])
+            del instances[key]
+            storage.save()
+        except KeyError:
+            print("** no instance found **")
+        except AttributeError:
+            print("** class doesn't exist **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances based or not on the class name."""
+        args = arg.split()
+        instances = []
+        if not arg:
+            instances.extend([str(obj) for obj in storage.all().values()])
+        else:
+            try:
+                instances.extend([str(obj) for obj in storage.all()[args[0]].values()])
+            except KeyError:
+                print("** class doesn't exist **")
+                return
+        print(instances)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding or updating attribute."""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        try:
+            instances = storage.all()
+            key = "{}.{}".format(args[0], args[1])
+            instance = instances[key]
+            setattr(instance, args[2], args[3])
+            instance.save()
+        except KeyError:
+            print("** no instance found **")
+        except AttributeError:
+            print("** class doesn't exist **")
+
+
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
 
