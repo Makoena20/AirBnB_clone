dd231c5518c11d71d205447399eebbd903118c3c
Makoena20 <koenaletseku@gmail.com>
2024-05-06 03:06:46 -0700

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,42 @@
+import uuid
+from datetime import datetime
+
 class BaseModel:
-    """Base model class"""
-
-    def __init__(self):
-        """Initializes the base model"""
-
-    def save(self):
-        """Save the model"""
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Convert the model to a dictionary"""
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
 
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-class TestBaseModel(unittest.TestCase):
-    """Test cases for the base model"""
+if __name__ == "__main__":
+    my_model = BaseModel()
+    print(my_model.id)
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key, value in my_model_json.items():
+        print("\t{}: ({}) - {}".format(key, type(value), value))
 
-    def setUp(self):
-        """Set up the test"""
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
 
-    def tearDown(self):
-        """Clean up after the test"""
